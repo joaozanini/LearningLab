b2c5881e1e92e0039435b4034306ee143619a6fc
João Vitor <joaozanini.profissional@gmail.com>
2024-10-16 11:01:41 -0300

    Create C# Project

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -10,6 +10,28 @@ namespace C_
     {
         static void Main(string[] args)
         {
+            // Hello World de lei
+            Console.WriteLine("Hello, world"); // da pra escrever cw e apertar tab que faz o console.writeline
+                // if not using System; System.Console.WriteLine("Hello, world");
+
+
+            // Declarando variáveis
+            int age = 18;
+
+            int idade;
+            idade = 18;
+            // podemos declarar uma váriavel e depois alocar dados ou fazer direto
+
+            // Data Types
+                //cw(tipo.MaxValue); ou cw(tipo.MinValue)
+            int integer = 1; // [int32]
+            long longNum = 900000000000000000L; // [int64]
+            double negative = -55.2D;
+            float precision = -55.002F;
+            decimal money = 14.99M;
+            
+                // podemos declarar várias variáveis de uma vez
+            int x = 20, y = 10, z = 30;
         }
     }
 }
