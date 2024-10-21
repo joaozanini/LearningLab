0ec17ff065a52e700a1faf5858c1dd34cd6ec520
João Vitor <joaozanini.profissional@gmail.com>
2024-10-21 14:18:09 -0300

    Update Program.cs

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -10,28 +10,31 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            // Hello World de lei
             Console.WriteLine("Hello, world"); // da pra escrever cw e apertar tab que faz o console.writeline
                 // if not using System; System.Console.WriteLine("Hello, world");
 
-
-            // Declarando variáveis
             int age = 18;
 
             int idade;
             idade = 18;
-            // podemos declarar uma váriavel e depois alocar dados ou fazer direto
 
-            // Data Types
-                //cw(tipo.MaxValue); ou cw(tipo.MinValue)
+            int x = 20, y = 10, z = 30;
+
+            //cw(tipo.MaxValue); ou cw(tipo.MinValue)
             int integer = 1; // [int32]
             long longNum = 900000000000000000L; // [int64]
             double negative = -55.2D;
             float precision = -55.002F;
             decimal money = 14.99M;
-            
-                // podemos declarar várias variáveis de uma vez
-            int x = 20, y = 10, z = 30;
+            string name = "Joao";
+            char letter = 'J';
+
+            string textAge = "-23";
+            int txtTo32value = Convert.ToInt32(textAge);
+
+            Console.WriteLine($"Olá, {name}");
+            Console.Read();
+
         }
     }
 }
