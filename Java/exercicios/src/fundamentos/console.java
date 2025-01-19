09a6cba8598ef2ac870c85562dc387c25b991042
João Vitor <joaozanini.profissional@gmail.com>
2025-01-19 15:00:47 -0300

    Create console.java

---
+++ Java/exercicios/src/fundamentos/console.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class console {
+	public static void main(String[] args) {
+		System.out.print("Digite seu nome: ");
+		Scanner entrada = new Scanner(System.in);
+		String nome = entrada.nextLine();
+		
+		System.out.println("Seu nome: " + nome);
+	}
+}
