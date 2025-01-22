4aa0f516dd9a17138af59186fc8da021824b8c2b
João Vitor <joaozanini.profissional@gmail.com>
2025-01-22 19:01:12 -0300

    C#

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -33,8 +33,20 @@ namespace C_
             int txtTo32value = Convert.ToInt32(textAge);
 
             Console.WriteLine($"Olá, {name}");
-            Console.Read();
 
+            double nota = 7.32D;
+            if (nota < 5)
+            {
+                Console.WriteLine("Reprovado");
+            } else if (nota > 5 && nota < 7)
+            {
+                Console.WriteLine("Recuperação");
+            } else if (nota > 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+
+            Console.Read();
         }
     }
 }
