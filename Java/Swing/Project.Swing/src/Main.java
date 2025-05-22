43b9822c4529966472f056650f942d65165d1a26
João Vitor <joaozanini.pessoal@gmail.com>
2025-05-21 21:14:15 -0300

    Update Main.java

--- Java/Swing/Project.Swing/src/Main.java
+++ Java/Swing/Project.Swing/src/Main.java
@@ -3,12 +3,13 @@ import java.awt.*;
 
 public class Main {
 
+    // esse arquivo é apenas um teste de Swing
     public static void main(String[] args) {
         JFrame frame = new JFrame("Tela");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
         frame.setLayout(new FlowLayout());
-        frame.setLocationRelativeTo(null); // null centraliza relativo a tela (Centraliza)
+        frame.setLocationRelativeTo(null);
 
         JButton button = new JButton("Clique Aqui!");
 
